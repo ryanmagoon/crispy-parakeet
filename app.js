2e452e90916188c1c33810818df646fe3409aed4
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-12 18:37:00 -0400

    add bootstrap template and static route

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var app = express();
 
 var port = 5000;
 
+app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
 app.get('/books', function(req, res) {
     res.send('Hello Books');
-})
+});
 app.listen(port, function(err) {
     console.log('running server on port ' + port);
 });
