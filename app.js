f03d64703c029392fd8c06ac9237f27b9aefbaae
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-13 00:15:05 -0400

    add bower

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-var express = require('express')
+var express = require('express');
 
 var app = express();
 
 var port = 5000;
 
 app.use(express.static('public'));
+app.use(express.static('src/views'));
+
 app.get('/', function(req, res) {
     res.send('Hello World');
 });
