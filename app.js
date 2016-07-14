a991092603630c50c734ab6ee92531a2c87223e9
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-14 14:21:42 -0400

    refactor nav to make code DRYer

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
-var bookRouter = require('./src/routes/bookRoutes');
+var nav = nav: [{
+    Link: '/Books',
+    Text: 'Book'},
+                {
+    Link: '/Authors',
+    Text: 'Author'
+    }];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
-
-
 
 app.use('/Books', bookRouter)
 
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,51 +1,36 @@
 var express = require('express');
-
 var bookRouter = express.Router();
-
-var books = [
-    {
-        title: 'You are beautiful',
-        genre: 'beautiful nonfiction',
-        author: 'Ryan Magoon',
-        read: false
-    },
-    {
-        title: 'Miracle on 34th street',
-        genre: 'nonfiction',
-        author: 'Ryan Magoon',
-        read: true
-    }
-];
-
-bookRouter.route('/')
-    .get(function(req, res) {
-    res.render('bookListView', {
-        title: 'Hello from render',
-        nav: [{
-            Link: '/Books',
-            Text: 'Books'},
-              {
-                  Link: '/Authors',
-                  Text: 'Authors'
-              }],
-        books: books
+var router = function (nav) {
+    var books = [
+        {
+            title: 'You are beautiful'
+            , genre: 'beautiful nonfiction'
+            , author: 'Ryan Magoon'
+            , read: false
+        }
+        , {
+            title: 'Miracle on 34th street'
+            , genre: 'nonfiction'
+            , author: 'Ryan Magoon'
+            , read: true
+        }
+    ];
+    bookRouter.route('/').get(function (req, res) {
+        res.render('bookListView', {
+            title: 'Hello from render',
+            nav: nav,
+            books: books
+        });
     });
-});
-
-bookRouter.route('/:id')
-    .get(function(req, res) {
-    var id = req.params.id;
-    res.render('bookView', {
-        title: 'Hello from render',
-        nav: [{
-            Link: '/Books',
-            Text: 'Books'},
-              {
-                  Link: '/Authors',
-                  Text: 'Authors'
-              }],
-        books: books[id]
+    bookRouter.route('/:id').get(function (req, res) {
+        var id = req.params.id;
+        res.render('bookView', {
+            title: 'Hello from render',
+            nav: nav,
+            book: books[id]
+        });
     });
-});
-
+    
+    return bookRouter;
+};
 module.exports = bookRouter;
