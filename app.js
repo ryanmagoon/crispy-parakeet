b68cc2e1e94347c2c14297f4e7ef9707bcb2a8b3
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-14 20:16:39 -0400

    hook up auth route to send JSON data on submit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
-var mongodb = require('mongodb').MongoClient;
 
 var port = process.env.PORT || 5000;
 var nav = [{
@@ -12,14 +16,19 @@ var nav = [{
     }];
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 
 app.get('/', function(req, res) {
