4611d1b03b93241fc9b3ff963510162b1924c2f6
Ryan Magoon <mr.magoon5@gmail.com>
2016-07-14 22:28:01 -0400

    implement user password verification against db

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,5 +1,6 @@
 var passport = require('passport'),
-    localStrategy = require('passport-local').Strategy;
+    localStrategy = require('passport-local').Strategy,
+    mongodb = require('mongodb').MongoClient;
 
 module.exports = function() {
     passport.use(new localStrategy({
@@ -7,10 +8,21 @@ module.exports = function() {
         passwordField: 'password'
     },
     function(username, password, done) {
-        var user = {
-            username: username,
-            password: password
-        };
-        done(null, user);
+        var url = 'mongodb://localhost:27017/libraryApp';
+        
+        mongodb.connect(url, function(err, db) {
+            var collection = db.collection('users');
+            collection.findOne({
+                username: username
+            },
+            function(err, results) {
+                if (results.password === password) {
+                    var user = results;
+                    done(null, user);
+                } else {
+                    done(null, false, { message: 'Bad password' });
+                }
+            });
+        })
     }));
 };
